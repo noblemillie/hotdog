0023a89231b85132f7e5807d029a6337d0071271
Noble Millie <noble.millie@gmail.com>
2016-04-01 10:47:04 -0500

    setup

---
+++ models/dawg.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var dawgSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  favoritetoy: String,
+  created_at: Date,
+  updated_at: Date
+});
+
+// Method to "say hello"
+userSchema.methods.sayWhatup = function() {
+  console.log("What's up " + this.name + '! Time to fetch some ' + this.favoritetoy + ', if you know what I\'\m sayin.');
+};
+
+
+var Dawg = mongoose.model('Dawg', dawgSchema);
+
+// Make this available to our other files
+module.exports = Dawg;
